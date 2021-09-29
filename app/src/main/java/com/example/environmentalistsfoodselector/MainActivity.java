c327bcca931987ce49cbf320cb50822fc24db3bd
shixianglim <slim2@ufl.edu>
2021-09-29 16:05:14 -0400

    Hello World CircleCI

--- app/src/main/java/com/example/environmentalistsfoodselector/MainActivity.java
+++ app/src/main/java/com/example/environmentalistsfoodselector/MainActivity.java
@@ -11,4 +11,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public String skeleton0(){
+        return "Test Skeleton0 Code";
+    }
+
+    public String skeleton1() {
+       return "Test Skeleton1 Code";
+
+    }
+    
+    public String skeleton2() {
+        return "Test Skeleton2 Code";
+    }
+
+
+
 }
--- app/src/test/java/com/example/environmentalistsfoodselector/ExampleUnitTest.java
+++ app/src/test/java/com/example/environmentalistsfoodselector/ExampleUnitTest.java
@@ -10,8 +10,20 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private MainActivity app = new MainActivity();
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testExample0(){ assertEquals("Test Skeleton0 Code", app.skeleton0());}
+    @Test
+    public void testExample1(){ assertEquals("Test Skeleton1 Code", app.skeleton1());}
+    @Test
+    public void testExample2(){ assertEquals("Test Skeleton2 Code", app.skeleton2());}
+
+    public void main(String[] test){
+
     }
 }

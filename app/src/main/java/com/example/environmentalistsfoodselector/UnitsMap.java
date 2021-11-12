6d39b0126c8384a85db7b949977a515b9841ec9e
phidoanUFL <phidoan@ufl.edu>
2021-11-11 19:43:06 -0600

    move food and unit map outside of main.java

---
+++ app/src/main/java/com/example/environmentalistsfoodselector/UnitsMap.java
@@ -0,0 +1,37 @@
+package com.example.environmentalistsfoodselector;
+
+import java.util.HashMap;
+
+public class UnitsMap {
+    private final HashMap<String, Unit> unitsMap;
+
+    /*
+      static UnitMap instance;
+
+      static UnitMap getInstance(){
+          if (instance== null)
+              instance = new UnitMap();
+          return instance;
+      }
+  */
+    public UnitsMap() {
+        unitsMap = new HashMap<String, Unit>();
+        // scale for weight units are relative to gram, scale for volume units are relative to mL
+        unitsMap.put( "g", new Unit( true, 1.0f ) );
+        unitsMap.put( "kg", new Unit( true, 1000.0f ) );
+        unitsMap.put( "oz", new Unit( true, 28.35f ) );
+        unitsMap.put( "lb", new Unit( true, 453.6f ) );
+        unitsMap.put( "mL", new Unit( false, 1.0f ) );
+        unitsMap.put( "L", new Unit( false, 1000.0f ) );
+        unitsMap.put( "tsp", new Unit( false, 4.93f ) );
+        unitsMap.put( "tbsp", new Unit( false, 14.79f ) );
+        unitsMap.put( "fl oz", new Unit( false, 29.57f ) );
+        unitsMap.put( "cup", new Unit( false, 236.59f ) );
+        unitsMap.put( "gallon", new Unit( false, 3785.41f ) );
+
+    }
+    public HashMap<String, Unit> getUnitsMap(){
+
+        return unitsMap;
+    }
+}

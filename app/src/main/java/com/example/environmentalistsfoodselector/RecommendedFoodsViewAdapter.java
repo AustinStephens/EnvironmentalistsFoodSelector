32cbd1336288717af4b1b967195e111238a27d83
Austin Stephens <30841341+AustinStephens@users.noreply.github.com>
2021-11-13 04:49:08 -0500

    Changed UI

--- app/src/main/java/com/example/environmentalistsfoodselector/RecommendedFoodsViewAdapter.java
+++ app/src/main/java/com/example/environmentalistsfoodselector/RecommendedFoodsViewAdapter.java
@@ -1,9 +1,11 @@
 package com.example.environmentalistsfoodselector;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,14 +13,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-public class RecommdenedFoodsViewAdapter extends RecyclerView.Adapter<RecommdenedFoodsViewAdapter.ViewHolder> {
+public class RecommendedFoodsViewAdapter extends RecyclerView.Adapter<RecommendedFoodsViewAdapter.ViewHolder> {
     private Food currentFood;
     private ArrayList<Food> data;
     private ArrayList<String> names;
     private LayoutInflater mInflater;
 
     // data is passed into the constructor
-    RecommdenedFoodsViewAdapter(Context context, ArrayList<Food> data, ArrayList<String> names, Food currentFood) {
+    RecommendedFoodsViewAdapter(Context context, ArrayList<Food> data, ArrayList<String> names, Food currentFood) {
         this.mInflater = LayoutInflater.from(context);
         this.data = data;
         this.names = names;
@@ -35,16 +37,21 @@ public class RecommdenedFoodsViewAdapter extends RecyclerView.Adapter<Recommdene
     // binds the data to the TextView in each row
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        int width = Resources.getSystem().getDisplayMetrics().widthPixels;
         Food food = data.get(position);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(70, 40, 0, 0);
+
         // if we passed in the current food object, meaning the list is empty
         if(currentFood.equals(data.get(position))) {
-            holder.nameView.setText("        Excellent Selection! No Better Foods Found!"); // looks weird without spaces, but may need to keep the same because this doesn't adjust to different screen sizes
+            lp.setMargins((width / 2) - 400, 40, 0, 0);
+            holder.nameView.setText("Excellent Selection! No Better Foods Found!"); // looks weird without spaces, but may need to keep the same because this doesn't adjust to different screen sizes
         }
         else {
             String name = names.get(position);
             holder.nameView.setText(name);
-            DecimalFormat f = new DecimalFormat("#.###");
+            DecimalFormat f = new DecimalFormat("#.##");
             // makes sure they line up with units
             if (currentFood.weight && food.weight) {
                 holder.carbonView.setText(f.format( food.carbonUsage[0] / currentFood.carbonUsage[0]*100) + "% less Carbon");
@@ -60,6 +67,7 @@ public class RecommdenedFoodsViewAdapter extends RecyclerView.Adapter<Recommdene
                     holder.waterView.setText(f.format( food.waterUsage[1] / currentFood.waterUsage[1]*100) + "% less water");
             }
         }
+        holder.nameView.setLayoutParams(lp);
     }
 
     // total number of rows

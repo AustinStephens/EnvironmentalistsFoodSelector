79ca5b829d2572e4364ddf2021290bcb1df45d05
Austin Stephens <30841341+AustinStephens@users.noreply.github.com>
2021-11-15 18:30:41 -0500

    Delete Button

--- app/src/main/java/com/example/environmentalistsfoodselector/AddedItemsViewAdapter.java
+++ app/src/main/java/com/example/environmentalistsfoodselector/AddedItemsViewAdapter.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -44,6 +45,14 @@ public class AddedItemsViewAdapter extends RecyclerView.Adapter<AddedItemsViewAd
         holder.amountText.setText(item.getAmountText());
         holder.carbonText.setText(item.getCarbonText(totalCarbon));
         holder.waterText.setText(item.getWaterText(totalWater));
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(width - 230, 8, 0, 0);
+        holder.deleteButton.setLayoutParams(lp);
+        FrameLayout.LayoutParams lp2 = new FrameLayout.LayoutParams(
+                400, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp2.setMargins(width - 680, 45, 0, 0);
+        holder.amountText.setLayoutParams(lp2);
     }
 
     // total number of rows
@@ -59,6 +68,7 @@ public class AddedItemsViewAdapter extends RecyclerView.Adapter<AddedItemsViewAd
         TextView carbonText;
         TextView waterText;
         TextView amountText;
+        Button deleteButton;
 
 
         ViewHolder(View itemView) {
@@ -67,12 +77,20 @@ public class AddedItemsViewAdapter extends RecyclerView.Adapter<AddedItemsViewAd
             carbonText = itemView.findViewById(R.id.carbonText);
             waterText = itemView.findViewById(R.id.waterText);
             amountText = itemView.findViewById(R.id.amountText);
+            deleteButton = itemView.findViewById(R.id.deleteButton);
+            deleteButton.setOnClickListener(this);
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-
+            if(view.equals(deleteButton)) {
+                int pos = getAdapterPosition();
+                items.remove(pos);
+                notifyDataSetChanged();
+                //notifyItemRemoved(pos);
+                //notifyItemRangeChanged(pos, items.size());
+            }
         }
     }
 }
